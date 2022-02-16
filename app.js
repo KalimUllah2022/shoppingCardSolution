77047baf737d6f1598803a8dadffdbb6e233ed4e
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-02-16 06:53:38 +0600

    commit

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-document.getElementById('case-plus').addEventListener('click', function() {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-});
+console.log('I am in a separate file')
